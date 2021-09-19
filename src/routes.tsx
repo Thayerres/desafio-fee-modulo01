07a54d72e388282b9a87358bee77c1fe8112af85
Thayerres Scarpini <thayerres@gmail.com>
2021-09-19 17:52:55 -0300

    feat: create path /:recipe

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,15 +2,19 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-  } from "react-router-dom";
-  
-  import Home from './pages/Home';
+} from "react-router-dom";
+
+import Home from './pages/Recipes';
+import Detais from "./pages/Details";
 
 const Routes:React.FC = () => {
     return (<Router>
         <Switch>
             <Route exact path="/">
                 <Home />
+            </Route>
+            <Route path="/:recipe">
+                <Detais />
             </Route>
         </Switch>
     </Router>
